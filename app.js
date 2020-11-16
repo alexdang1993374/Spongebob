4776a4ddde77a55611e5776f8cd3d7536f491e9c
Alex Dang <alexdang1993374@gmail.com>
2020-11-17 02:18:40 +0700

    onload

--- app.js
+++ app.js
@@ -1,4 +1,45 @@
-var audio;
+let audio;
+let audioLeg;
+let audioFirm;
+let audioReady;
+let audioGo;
+let audioChocolate;
+let audioWumbo;
+let audioNumber;
+let audioStinks;
+let audioPeople;
+let audioClaws;
+let audioSlasher;
+let audioFuture;
+let audioArray = []
+window.onload = () => {
+  audioLeg = new Audio("./sounds/leg.wav");
+  audioFirm = new Audio("./sounds/firm.wav");
+  audioReady = new Audio("./sounds/areYouReady.wav");
+  audioGo = new Audio("./sounds/imReady.wav");
+  audioChocolate = new Audio("./sounds/chocolate.wav");
+  audioWumbo = new Audio("./sounds/wumbo.wav");
+  audioNumber = new Audio("./sounds/numberOne.wav");
+  audioStinks = new Audio("./sounds/stinks.wav");
+  audioPeople = new Audio("./sounds/people.wav");
+  audioClaws = new Audio("./sounds/claws.wav");
+  audioSlasher = new Audio("./sounds/slasher.wav");
+  audioFuture = new Audio("./sounds/future.wav");
+  audioArray.push(audioFirm)
+  audioArray.push(audioReady)
+  audioArray.push(audioGo)
+  audioArray.push(audioChocolate)
+  audioArray.push(audioWumbo)
+  audioArray.push(audioNumber)
+  audioArray.push(audioStinks)
+  audioArray.push(audioPeople)
+  audioArray.push(audioLeg)
+  audioArray.push(audioClaws)
+  audioArray.push(audioSlasher)
+  audioArray.push(audioFuture)
+}
+
+
 function playRandom() {
   var queenUrls = [
     "blairStClair",
@@ -22,7 +63,8 @@ function play(url) {
   if (audio) {
     audio.pause();
   }
-  audio = new Audio("./sounds/" + url + ".wav");
+  // audio = new Audio("./sounds/" + url + ".wav");
+  audio = audioArray[url]
   audio.play();
 }
 
