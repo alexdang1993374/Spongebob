c81f99cea653cce87a64a8e4df69eb0572ae38db
Alex Dang <alexdang1993374@gmail.com>
2020-11-16 15:00:03 +0700

    soundboard

---
+++ app.js
@@ -0,0 +1,28 @@
+var audio;
+function playRandom() {
+  var queenUrls = [
+    "blairStClair",
+    "detox",
+    "jinkx",
+    "karmeronMicheals",
+    "kennedyDavenport",
+    "mayhem",
+    "missel",
+    "sashaVelour",
+    "sharon",
+    "theTuck",
+    "valentina",
+    "vangie",
+  ];
+  var randomIndex = Math.floor(Math.random() * queenUrls.length);
+  var randomQueenUrl = queenUrls[randomIndex];
+  play(randomQueenUrl);
+}
+function play(url) {
+  if (audio) {
+    audio.pause();
+  }
+  audio = new Audio("./sounds/" + url + ".wav");
+  audio.play();
+}
+
